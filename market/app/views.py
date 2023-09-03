47f4fea1eca96f8e5d798785e20f006d76e573dd
Mostakim Jihad <mostakimjihad652001@gmail.com>
2023-09-03 18:34:21 +0600

    csv data loaded to mySQL server

---
+++ market/app/views.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from django.shortcuts import render, HttpResponse
+from app.models import Data
+
+# Create your views here.
+
+def index(request):
+
+    df = pd.read_csv("app/stock_market_data.csv")
+    for _, row in df.iterrows():
+        Data.objects.create(
+            date=row['date'],
+            trade_code=row['trade_code'],
+            high=row['high'],
+            low=row['low'],
+            open=row['open'],
+            close=row['close'],
+            volume=row['volume']
+        )
+
+    data = Data.objects.all()
+    context = {
+        'data': data,
+    }
+    return render(request, "index.html", context)
